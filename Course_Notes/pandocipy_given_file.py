61e9b09488c2443a829ffdb159265e75faa34c46
drvinceknight <vincent.knight@gmail.com>
2014-01-21 08:36:11 +0000

    Not sure whats going on

--- Course_Notes/pandocipy_given_file.py
+++ Course_Notes/pandocipy_given_file.py
@@ -2,13 +2,21 @@
 from sys import argv
 from os import system
 
+mediaserver = "http:\/\/drvinceknight.github.io\/Year\_3\_game\_theory\_course\/Course\_Notes\/"
+
 e = argv[1][:-3]
 print e
 
+# HTML
 system("sed 's/.md/.html/g' %s > tmp" % (e + ".md"))
-system("pandoc -s tmp -N -o " + e + ".html --mathjax")
-system("sed 's/.md/.docx/g' %s > tmp" % (e + ".md"))
-system("pandoc tmp -o " + e + ".docx")
+system("sed 's/\!\[[.*\]/\!\[\]/g' tmp > tmp1")  # Get rid of captions
+system("sed 's/\\)\\[/\\)\\[%s/g' tmp > tmp1" % mediaserver)  # Change media server to github
+system("pandoc -s tmp1 -N -o " + e + ".html --mathjax")
+#system("sed 's/.md/.docx/g' %s > tmp" % (e + ".md"))
+#system("pandoc tmp -o " + e + ".docx")
+
+# PDF
 system("sed 's/.md/.pdf/g' %s > tmp" % (e + ".md"))
 system("pandoc tmp -N -o " + e + ".pdf --latex-engine=xelatex")
 system("rm tmp")
+system("rm tmp1")
