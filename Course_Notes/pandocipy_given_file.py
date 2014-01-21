832af4bfb84d943a5dc0dec5b790138fea098119
drvinceknight <vincent.knight@gmail.com>
2014-01-21 08:37:00 +0000

    Not sure whats going on

--- Course_Notes/pandocipy_given_file.py
+++ Course_Notes/pandocipy_given_file.py
@@ -10,6 +10,7 @@ print e
 # HTML
 system("sed 's/.md/.html/g' %s > tmp" % (e + ".md"))
 system("sed 's/\!\[[.*\]/\!\[\]/g' tmp > tmp1")  # Get rid of captions
+print "sed 's/\\)\\[/\\)\\[%s/g' tmp > tmp1" % mediaserver
 system("sed 's/\\)\\[/\\)\\[%s/g' tmp > tmp1" % mediaserver)  # Change media server to github
 system("pandoc -s tmp1 -N -o " + e + ".html --mathjax")
 #system("sed 's/.md/.docx/g' %s > tmp" % (e + ".md"))
