9a6b72b7ba435f3170da5c3462c1b5ea466b463c
drvinceknight <vincent.knight@gmail.com>
2014-01-21 08:57:01 +0000

    Having everything working great in html

--- Course_Notes/pandocipy_given_file.py
+++ Course_Notes/pandocipy_given_file.py
@@ -9,8 +9,8 @@ print e
 
 # HTML
 system("sed 's/.md/.html/g' %s > tmp" % (e + ".md"))
-system("sed 's/\!\[[.*\]/\!\[\]/g' tmp > tmp1")  # Get rid of captions
-system("sed 's/](/](%s/g' tmp > tmp1" % mediaserver)  # Change media server to github
+system("sed 's/\!\[.*\]/\!\[\]/g' tmp > tmp1")  # Get rid of captions
+system("sed 's/](/](%s/g' tmp1 > tmp" % mediaserver)  # Change media server to github
 system("pandoc -s tmp1 -N -o " + e + ".html --mathjax")
 #system("sed 's/.md/.docx/g' %s > tmp" % (e + ".md"))
 #system("pandoc tmp -o " + e + ".docx")
