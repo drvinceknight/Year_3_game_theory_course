d9891e54cb9b3663586cb45a5935cbe4374713fe
drvinceknight <vincent.knight@gmail.com>
2014-01-28 13:24:37 +0000

    Analysis for matching pennies game done.

--- Activities/Data/analyse.py
+++ Activities/Data/analyse.py
@@ -4,6 +4,7 @@ Usage:
     analyse.py <file>
 
 """
+from __future__ import division
 from csv import reader
 import matplotlib.pyplot as plt
 
@@ -18,9 +19,77 @@ class MatchingPennies():
         self.data = [row for row in reader(open(f, 'r'))]
     def analyse(self):
         self.dictionaries = [{'H':0, 'T':0}, {'H':0, 'T':0}, {'H':0, 'T':0}, {'H':0, 'T':0}]
+        self.mixedstrategiesgame1 = [[0,0]]
+        self.mixedstrategiesgame2 = [[0,0]]
+
+        self.scoresgame1 = [[0,0]]
+        self.scoresgame2 = [[0,0]]
+
         for row in self.data[1:]:
             for d in range(len(self.dictionaries)):
                 self.dictionaries[d][row[d]] += 1
+            self.mixedstrategiesgame1.append([self.mixedstrategiesgame1[-1][0] + int(row[0] == 'H'), self.mixedstrategiesgame1[-1][1] + int(row[1] == 'H')])
+            self.mixedstrategiesgame2.append([self.mixedstrategiesgame2[-1][0] + int(row[2] == 'H'), self.mixedstrategiesgame2[-1][1] + int(row[3] == 'H')])
+            if row[0] == row[1]:
+                self.scoresgame1.append([self.scoresgame1[-1][0] + 1, self.scoresgame1[-1][1] - 1])
+            else:
+                self.scoresgame1.append([self.scoresgame1[-1][0] - 1, self.scoresgame1[-1][1] + 1])
+
+            if row[2] == row[3]:
+                self.scoresgame2.append([self.scoresgame2[-1][0] + int(row[2] == 'H') + 1, self.scoresgame2[-1][1] - (1 + int(row[2] == 'H'))])
+            else:
+                self.scoresgame2.append([self.scoresgame2[-1][0] - int(row[2] == 'H') - 1 , self.scoresgame2[-1][1] + (1 + int(row[2] == 'H'))])
+
+        self.nbrofgames = len(self.mixedstrategiesgame1)
+        for k in range(1, self.nbrofgames + 1):
+            self.mixedstrategiesgame1[k - 1] = [i/k for i in self.mixedstrategiesgame1[k - 1]]
+            self.mixedstrategiesgame2[k - 1] = [i/k for i in self.mixedstrategiesgame2[k - 1]]
+            self.scoresgame1[k - 1] = [i/k for i in self.scoresgame1[k - 1]]
+            self.scoresgame2[k - 1] = [i/k for i in self.scoresgame2[k - 1]]
+
+        plt.figure()
+        x = range(1, self.nbrofgames + 1)
+        y = [i[0] for i in self.scoresgame1]
+        plt.plot(x,y, label="Player 1: Mean score")
+        y = [i[1] for i in self.scoresgame1]
+        plt.plot(x,y, label="Player 2: Mean score", color='red')
+        plt.legend(loc=4)
+        plt.savefig('meanscroresgame1.png')
+
+        plt.figure()
+        x = range(1, self.nbrofgames + 1)
+        y = [i[0] for i in self.scoresgame2]
+        plt.plot(x,y, label="Player 1: Mean score")
+        y = [i[1] for i in self.scoresgame2]
+        plt.plot(x,y, label="Player 2: Mean score", color='red')
+        plt.legend(loc=4)
+        plt.savefig('meanscroresgame2.png')
+
+        plt.figure()
+        x = range(1, self.nbrofgames + 1)
+        y = [i[0] for i in self.mixedstrategiesgame1]
+        plt.plot(x,y, label="Player 1: Probability of playing 'H'")
+        x = range(1, self.nbrofgames + 1)
+        y = [i[1] for i in self.mixedstrategiesgame1]
+        plt.plot(x,y, label="Player 2: Probability of playing 'H'", color='red')
+        y = [.5 for i in self.mixedstrategiesgame1]
+        plt.plot(x, y, 'g--')
+        plt.legend(loc=4)
+        plt.savefig('mixedstrategiesgame1.png')
+
+        plt.figure()
+        x = range(1, self.nbrofgames + 1)
+        y = [i[0] for i in self.mixedstrategiesgame2]
+        plt.plot(x,y, label="Player 1: Probability of playing 'H'")
+        x = range(1, self.nbrofgames + 1)
+        y = [i[1] for i in self.mixedstrategiesgame2]
+        plt.plot(x,y, label="Player 2: Probability of playing 'H'", color='red')
+        y = [1/3 for i in self.mixedstrategiesgame1]
+        plt.plot(x, y, 'c--')
+        y = [1/2 for i in self.mixedstrategiesgame1]
+        plt.plot(x, y, 'm--')
+        plt.legend(loc=4)
+        plt.savefig('mixedstrategiesgame2.png')
 
 
 
@@ -40,3 +109,4 @@ if __name__ == '__main__':
     a = returnclass(arguments['<file>'])
     a.analyse()
     print a.dictionaries
+    print a.mixedstrategiesgame2[-1]
